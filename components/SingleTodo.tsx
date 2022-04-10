3dd71d8b919949573a29e29ab7ef9e49058b367b
szaszfaijulia <szaszfai.julia@gmail.com>
2022-04-11 01:50:24 +0200

    1 file modified

--- components/SingleTodo.tsx
+++ components/SingleTodo.tsx
@@ -9,20 +9,33 @@ type Props = {
 };
 
 const SingleTodo = ({ todo, todos, setTodos }: Props) => {
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+
   return (
     <form className="todos__single">
-      <span className="todos__single--text">{todo.todo}</span>
+      {todo.isDone ? (
+        <s className="todos__single--text">{todo.todo}</s>
+      ) : (
+        <span className="todos__single--text">{todo.todo}</span>
+      )}
       <div>
         <span className="icon">Edit</span>
         <span className="icon">Delete</span>
-        <span className="icon">Done</span>
+        <span className="icon" onClick={() => handleDone(todo.id)}>
+          Done
+        </span>
       </div>
     </form>
   );
 };
 
 export default SingleTodo;
-
 
 //import { AiFillEdit, AiFillDelete } from 'react-icons/ai';
 //import { MdDone } from "react-icons/md";
